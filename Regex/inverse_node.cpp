fad753fdd67f519abe2b6bb093f131e9aef04885
Saurav Ghosh <71978046+thetanmoy@users.noreply.github.com>
2024-03-19 22:18:02 -0500

    Update inverse_node.cpp

--- Regex/inverse_node.cpp
+++ Regex/inverse_node.cpp
@@ -10,8 +10,16 @@ InverseNode::InverseNode(RegexNode* _node)
 
 
 // attempt to match the string at position pos
-bool InverseNode::match(const std::string& str, size_t &pos)
-{
-  // YOUR CODE HERE
-  return false;
+bool InverseNode::match(const std::string& str, size_t &pos) {
+    size_t originalPos = pos;
+
+    bool matchResult = _node->match(str, pos);
+
+    
+    if(matchResult) {
+        pos = originalPos; // revert the position
+        return false; // inverse logic
+    }
+
+    return true;
 }
